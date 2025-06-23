d79516f8840657e85681ca9041ca33395fa822f5
kukjinkim <tlaruddo2@gmail.com>
2025-06-22 19:19:53 -0700

    2025-06-22

--- 2025/kukjin/wk3/2025-06-22-valid-parenthese.py
+++ 2025/kukjin/wk3/2025-06-22-valid-parenthese.py
@@ -1,17 +1,24 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)  # Get the number of days
-        answer = [0] * n  # Initialize the result array with 0s
-        stack = []  # Stack to keep indices of days
+class Solution: 
+    def isValid(self, s: string) -> bool: 
+        stack = []
+        pairs = {')':'(', '}':'{',']':'['}
 
-        for i in range(n):  # Iterate through each day
-            while stack and temperatures[i] > temperatures[stack[-1]]:
-                # Compare current temp with the top of the stack
-                prev_day = stack.pop()
-                # Pop until current temperature is not greater than stack top
-                answer[prev_day] = i - prev_day
-                # Store the number of days waited
-            stack.append(i)
-            # Push current day index to stack for future comparison
+        for ch in s: 
+            #({[
+            if ch not in pairs:
+                stack.append(ch)
 
-        return answer  # Return the final result array
+
+            # }])
+            else: 
+                if not stack: 
+                    return False 
+
+                if stack.pop() != pairs[ch]:
+                    return False
+
+        if not stack:
+            return  True
+        else:
+            return False                                           
+
